2d4d3fe07bc5b3c2897e63be6ffdd9d4e0adb254
Zadfar <zadfarzubair55@gmail.com>
2023-09-28 20:21:29 +0530

    Built the Mobile Menu.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ collapsibles.forEach((item) =>
         this.classList.toggle("collapsible--expanded");
 })
 );
+
+const menu_btn = document.querySelector('.mobile-menu-btn');
+const mobile_menu = document.querySelector('.mobile-menu');
+const film = document.querySelector('.film');
+
+menu_btn.addEventListener('click', function () {
+    mobile_menu.classList.toggle('is-active');
+    menu_btn_c.classList.toggle('is-visible');
+    film.classList.toggle('is-visible');
+});
+
+const menu_btn_c =document.querySelector('.mobile-menu-btn-c');
+
+menu_btn_c.addEventListener('click', function () {
+    mobile_menu.classList.toggle('is-active');
+    menu_btn_c.classList.toggle('is-visible');
+    film.classList.toggle('is-visible');
+});
